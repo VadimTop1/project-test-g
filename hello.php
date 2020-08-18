dd2d19da093e2410d5d88456c52f9f092e0730c6
Vadim <vadim_1423@mail.ru>
2020-08-18 04:52:22 +0300

    Обновление страниц

--- hello.php
+++ hello.php
@@ -7,9 +7,12 @@
 <body>
 
 <h2>Страница теста аналитики <h1>[ГЛАВНАЯ]</h1></h2>
+<br/><br/>
 <table>
   <p><tr><td><a href="/page1.php">Пункт 1</a></td><td><a href="/page2.php">Пункт 2</a></td></tr></p>
 </table>
+<br/>
+<div id="tel1Id"><b>Телефон 1:</b> 001278989</div>
 <br/>
   <form name="f1" method="get" action="enter_data.php">
     <input name="link" type="hidden" value="index.php" />
@@ -37,3 +40,5 @@
             top: 50;
             right: 50px;"></div>
 </body>
+
+<div id="tel2Id"><b>Телефон 1:</b> 001278989</div>
