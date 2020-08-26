0090668a8bcc722e32a057b982f31ccb0f338065
Vadim <vadim_1423@mail.ru>
2020-08-26 14:29:30 +0300

    Подключение Google Tag Manager

--- hello.php
+++ hello.php
@@ -2,6 +2,8 @@
 
 ?>
 <head>
+    <title>Аналитик тест</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
     <!-- Google Tag Manager -->
     <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
     new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
@@ -9,8 +11,6 @@
     'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
     })(window,document,'script','dataLayer','GTM-KPMLLHJ');</script>
     <!-- End Google Tag Manager -->
-    <title>Аналитик тест</title>
-    <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
 
