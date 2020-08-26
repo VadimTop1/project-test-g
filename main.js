c42ddae7ed1df321ba1f8c43136d69a0cf598f22
Vadim <vadim_1423@mail.ru>
2020-08-26 17:53:01 +0300

    Стилизация страницы

--- main.js
+++ main.js
@@ -5,8 +5,12 @@ let formBoardStyle = getComputedStyle(form_board);
 buttonform.addEventListener( "click" , 
     function() {
         if(form_board.style.display == "none" || formBoardStyle.display == "none")
+        {
             form_board.style.display="block";
-        else
+            button_form.style.color = "#ffffff";
+        }else{
             form_board.style.display="none";
+            button_form.style.color = "#000000";
+        }
     }
 );
