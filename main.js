2a6c705fb36a10e8e50b795509c68af9c43642f2
Vadim <vadim_1423@mail.ru>
2020-08-26 05:32:49 +0300

    Обновление

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
 var buttonform = document.querySelector('button_form');
+var formBoard = document.querySelector('form_board');//form_board
+let formBoardStyle = getComputedStyle(formBoard);
+buttonform.addEventListener( "click" , 
+    function() {
+        if(formBoard.style.display == "none" || formBoardStyle.display == "none")
+            formBoard.style.display="block";
+        else
+            formBoard.style.display="none";
+    }
+);
