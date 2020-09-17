cd8cb25ae2f431f0a59818e9dc859823ee37c3e6
Vadim <vadim_1423@mail.ru>
2020-09-17 21:28:42 +0300

    Отправка email

--- form.php
+++ form.php
@@ -9,47 +9,14 @@ if(
 {
     echo '<br/><br/>'.$_POST['pdf2'].' | '.$_POST['name'].' | '.$_POST['mail'].'<br/>';
 
-    // несколько получателей
-    $to = $_POST['mail']; // обратите внимание на запятую
+    // Сообщение
+    $message = "Line 1\r\nLine 2\r\nLine 3";
 
-    // тема письма
-    $subject = 'Имя: '.$_POST['name'];
-
-    // текст письма
-    $message = '
-    <html>
-    <head>
-    <title>Test отправка</title>
-    </head>
-    <body>
-    <p>Here are the birthdays upcoming in August!</p>
-    <table>
-        <tr>
-        <th>Person</th><th>Day</th><th>Month</th><th>Year</th>
-        </tr>
-        <tr>
-        <td>Johny</td><td>10th</td><td>August</td><td>1970</td>
-        </tr>
-        <tr>
-        <td>Sally</td><td>17th</td><td>August</td><td>1973</td>
-        </tr>
-    </table>
-    </body>
-    </html>
-    ';
-
-    // Для отправки HTML-письма должен быть установлен заголовок Content-type
-    $headers  = 'MIME-Version: 1.0' . "\r\n";
-    $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-
-    // Дополнительные заголовки
-    //$headers[] = 'To: Mary <mary@example.com>, Kelly <kelly@example.com>';
-    //$headers[] = 'From: Birthday Reminder <birthday@example.com>';
-    //$headers[] = 'Cc: birthdayarchive@example.com';
-    //$headers[] = 'Bcc: birthdaycheck@example.com';
+    // На случай если какая-то строка письма длиннее 70 символов мы используем wordwrap()
+    $message = wordwrap($message, 70, "\r\n");
 
     // Отправляем
-    if(mail($to, $subject, $message, $headers))
+    if( mail('caffeinated@example.com', 'My Subject', $message))
         echo "<br/>Отправленно!!";
     else
         echo "<br/>Ошибка!!";
