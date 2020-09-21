8acb9f0ecd230f16f07df26b798ce0207021640a
Vadim <vadim_1423@mail.ru>
2020-09-21 03:02:20 +0300

    Отправка email с помощью phpmailer

--- form.php
+++ form.php
@@ -1,4 +1,5 @@
 <?php
+require 'phpmailer/config_mail.php';
 
 if(
     isset($_POST['pdf2']) &&
@@ -7,19 +8,20 @@ if(
     isset($_POST['enter'])
     )
 {
+    $name = $_POST['name'];
+    $mail = $_POST['mail'];
     echo '<br/><br/>'.$_POST['pdf2'].' | '.$_POST['name'].' | '.$_POST['mail'].'<br/>';
 
-    // Сообщение
-    $message = "Line 1\r\nLine 2\r\nLine 3";
+    // Формирование самого письма
+    $title = "Заголовок письма";
+    $body = "
+        <h2>Новое письмо</h2>
+        <b>Имя:</b> $name<br>
+        <b>Почта:</b> $email<br><br>
+    ";
 
-    // На случай если какая-то строка письма длиннее 70 символов мы используем wordwrap()
-    $message = wordwrap($message, 70, "\r\n");
-
-    // Отправляем
-    if( mail('caffeinated@example.com', 'My Subject', $message))
-        echo "<br/>Отправленно!!";
-    else
-        echo "<br/>Ошибка!!";
+    $mail = new Mail($body,  $title);
+    $mail->inf();
 }else
 {
     echo '<br/><br/>Введите все данные!!!<br/>';
---
+++ phpmailer/config_mail.php
@@ -0,0 +1,74 @@
+<?php
+
+// Файлы phpmailer
+require 'phpmailer/PHPMailer.php';
+require 'phpmailer/SMTP.php';
+require 'phpmailer/Exception.php';
+
+class Mail
+{
+    //private $body;  //Тело письма
+    //private $title; //Заголовок письма
+    private $result;
+    private $status;
+    public function __construct(string $body, string $title)
+    {
+        if(!isset($body)|| !isset($title))
+        return;
+        // Настройки PHPMailer
+        $mail = new PHPMailer\PHPMailer\PHPMailer();
+        try {
+            $mail->isSMTP();
+            $mail->CharSet = "UTF-8";
+            $mail->SMTPAuth   = true;
+            //$mail->SMTPDebug = 2;
+            $mail->Debugoutput = function($str, $level) {$GLOBALS['status'][] = $str;};
+
+            // Настройки вашей почты
+            $mail->Host       = 'ssl://smtp.mail.ru'; // SMTP сервера вашей почты
+            $mail->Username   = 'vadim2_1423@mail.ru'; // Логин на почте
+            $mail->Password   = 'agdWGHwc@s07d2sr'; // Пароль на почте
+            $mail->SMTPSecure = 'ssl';
+            $mail->Port       = 465;
+            $mail->setFrom('vadim2_1423@mail.ru', 'Вадим'); // Адрес самой почты и имя отправителя
+
+            // Получатель письма
+            $mail->addAddress('vadim_1423@mail.ru');
+            $mail->addAddress('skorpion.mega.krut@gmail.com'); // Ещё один, если нужен
+
+            // Прикрипление файлов к письму
+            //if (!empty($file['name'][0])) {
+            //    for ($ct = 0; $ct < count($file['tmp_name']); $ct++) {
+            //        $uploadfile = tempnam(sys_get_temp_dir(), sha1($file['name'][$ct]));
+            //        $filename = $file['name'][$ct];
+            //        if (move_uploaded_file($file['tmp_name'][$ct], $uploadfile)) {
+            //            $mail->addAttachment($uploadfile, $filename);
+            //            $rfile[] = "Файл $filename прикреплён";
+            //        } else {
+            //            $rfile[] = "Не удалось прикрепить файл $filename";
+            //        }
+            //    }
+            //}
+            //$rfile[] = "нету";
+            // Отправка сообщения
+            $mail->isHTML(true);
+            $mail->Subject = $title;
+            $mail->Body = $body;
+
+            // Проверяем отравленность сообщения
+            if ($mail->send())
+                $this->result = "success";
+            else
+                $this->result = "error";
+
+        } catch (Exception $e) {
+            $this->result = "error";
+            $this->status = "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
+        }
+    }
+
+    public function inf()
+    {
+        echo json_encode(["result" => $this->result, /*"resultfile" => $rfile,*/ "status" => $this->status]);
+    }
+}
