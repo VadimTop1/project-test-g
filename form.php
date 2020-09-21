c453b6150e0da6a0fe028bcb9f9f7ee1d8d80c6e
Vadim <vadim_1423@mail.ru>
2020-09-21 10:16:49 +0300

    Отправка email с помощью phpmailer

--- form.php
+++ form.php
@@ -1,9 +1,9 @@
 <?php
 //require 'phpmailer/config_mail.php';
 echo '<p>test0</p>';
-require 'PHPMailer\PHPMailer\PHPMailer.php';
+require 'PHPMailer/PHPMailer/PHPMailer.php';
 echo '<p>test1</p>';
-require 'PHPMailer\PHPMailer\SMTP.php';
+require 'PHPMailer/PHPMailer/SMTP.php';
 echo '<p>test2</p>';
 require 'PHPMailer\PHPMailer\Exception.php';
 echo '<p>test3</p>';
