94b75b9fcbbbdc705f53b6db23278eb4434a1f56
Vadim <vadim_1423@mail.ru>
2021-01-07 00:32:21 +0300

    Новый google anlitics 4, Тест

--- page1.php
+++ page1.php
@@ -2,12 +2,16 @@
 ?>
 <head>
     <!-- Google Tag Manager -->
+    <!-- Текущая версия: GTM-PXQ8ST9 -->
+    <!-- Старая версия: GTM-KPMLLHJ-->
     <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
     new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
     j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
     'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','GTM-KPMLLHJ');</script>
+    })(window,document,'script','dataLayer','GTM-PXQ8ST9');</script>
     <!-- End Google Tag Manager -->
+
+
     <title>Аналитик тест</title>
 
     <link rel="stylesheet" type="text/css" href="style.css">
@@ -15,9 +19,12 @@
 <body>
 
     <!-- Google Tag Manager (noscript) -->
-    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KPMLLHJ"
+    <!-- Текущая версия: id=GTM-PXQ8ST9 -->
+    <!-- Старая версия: id=GTM-KPMLLHJ -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PXQ8ST9"
     height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
     <!-- End Google Tag Manager (noscript) -->
+
 
     <p><h2>Страница теста аналитики <h1>[PAGE 1]</h1></h2></p>
     <table>
